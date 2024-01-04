3934e345ca3517af7c0781a2ae6ab8470344fe76
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-01-04 19:34:44 +0530

    Delete python new work/bank.py

--- python new work/bank.py
+++
@@ -1,6 +0,0 @@
-# for using "f"string 
-bank_name="sbi"
-account_number="1234"
-amount="34000"
-#your sbi account 1234 has been credited with amount 34000
-print(f"your {bank_name} account {account_number} has been credited with amount {amount}")
