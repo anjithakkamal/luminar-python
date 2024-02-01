f19ad16fef003624ef3a15e6162ccbe6bb30b748
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-02-01 20:07:56 +0530

    Add files via upload

--- python new work/regular expression/phone.py
+++ python new work/regular expression/phone.py
@@ -1,5 +1,5 @@
 from re import *
-phone_number="9947374509"
-rule="[0-9]{10}"
+phone_number="+919947374509"
+rule="(\+91)?[0-9]{10}"
 matcher=fullmatch(rule,phone_number)
 print("invalid" if matcher == None else "valid")
--- python new work/regular expression/vehiclereg.py
+++ python new work/regular expression/vehiclereg.py
@@ -1,5 +1,6 @@
 from re import *
-vehicle_number="KL05TH6479"
-rule="(KL)[0-9]{2}[A-Z]{1,2}[0-9]{4}"
+vehicle_number="KL-05-TH-6479"
+rule="(KL)[-]?[0-9]{2}[-]?[A-Z]{1,2}[-]?[0-9]{4}"     #[-]? for '-?'is optional
+# rule="(KL)[0-9]{2}[A-Z]{1,2}[0-9]{4}"
 matcher=fullmatch(rule,vehicle_number)
 print("invalid" if matcher == None else "valid")
