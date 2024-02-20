36a7ccee58478865815c6e88ac96779d864c40ad
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-02-20 18:19:08 +0530

    Add files via upload

--- python new work/TASK 6/q13.py
+++ python new work/TASK 6/q13.py
@@ -1,5 +1,5 @@
 # Python program to print the elements of an array present on odd position 
 arr = [2,34,56,3,43,23,11,99];   
 print("elements of an array present on odd position")
-for i in range(0, len(arr), 2):  
+for i in range(1, len(arr), 2):  
     print(arr[i])
