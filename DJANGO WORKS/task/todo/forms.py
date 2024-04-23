6b3d55f7f166eca343e4cd40017ee0325e7f92fb
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-04-23 19:58:40 +0530

    Add files via upload

--- DJANGO WORKS/task/todo/forms.py
+++ DJANGO WORKS/task/todo/forms.py
@@ -28,8 +28,17 @@ class RegistrationForm(forms.ModelForm):
 
         fields=["username","email","password"]
 
+        widgets={
+
+            "username":forms.TextInput(attrs={"class":"form-control"}),
+
+            "email":forms.EmailInput(attrs={"class":"form-control"}),
+
+            "password":forms.PasswordInput(attrs={"class":"form-control"})
+        }
+
 class LoginForm(forms.Form):
 
-    username=forms.CharField()
+    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    password=forms.CharField()
+    password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
