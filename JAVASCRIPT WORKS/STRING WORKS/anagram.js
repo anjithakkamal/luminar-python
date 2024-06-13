fed39a2471b9de1a0519d6b57640260397074e72
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-06-13 10:04:31 +0530

    Add files via upload

---
+++ JAVASCRIPT WORKS/STRING WORKS/anagram.js
@@ -0,0 +1,12 @@
+
+var t1="listen"
+var t2="silent"
+srt_t1=t1.split('').sort().join()
+srt_t2=t2.split('').sort().join()
+if(srt_t1==srt_t2){
+    console.log("anagaram");
+}
+else{
+    console.log("not anagram");
+}
+
