c0dfa461e80e3d9e8ac40c7c6b60648f4e720182
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-06-20 19:50:38 +0530

    Add files via upload

---
+++ JAVASCRIPT WORKS/DOM WORKS/SimpleCalculator/script.js
@@ -0,0 +1,43 @@
+function performAddition(){
+
+    let num1=+document.querySelector("#box1").value
+
+    let num2=+document.querySelector("#box2").value
+
+    let result=num1+num2
+
+    console.log(result);
+}
+
+function performSubtraction(){
+
+    let num1=+document.querySelector("#box1").value
+
+    let num2=+document.querySelector("#box2").value
+
+    let result=num1-num2
+
+    console.log(result);
+}
+
+function performMultiplication(){
+
+    let num1=+document.querySelector("#box1").value
+
+    let num2=+document.querySelector("#box2").value
+
+    let result=num1*num2
+
+    console.log(result);
+}
+
+function performDivision(){
+
+    let num1=+document.querySelector("#box1").value
+
+    let num2=+document.querySelector("#box2").value
+
+    let result=num1/num2
+    
+    console.log(result);
+}
--- JAVASCRIPT WORKS/STRING WORKS/anagram.js
+++ JAVASCRIPT WORKS/STRING WORKS/anagram.js
@@ -1,12 +1,5 @@
-
 var t1="listen"
 var t2="silent"
-srt_t1=t1.split('').sort().join()
-srt_t2=t2.split('').sort().join()
-if(srt_t1==srt_t2){
-    console.log("anagaram");
-}
-else{
-    console.log("not anagram");
-}
-
+srt_t1=t1.split('').sort().join('')
+srt_t2=t2.split('').sort().join('')
+console.log(srt_t1==srt_t2 ?"anagram":"not anagram" );
