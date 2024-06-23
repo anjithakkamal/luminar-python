57ab5734f6638e9b3397796b6a40bccb7aea4c22
anjithakkamal <74664453+anjithakkamal@users.noreply.github.com>
2024-06-23 21:12:38 +0530

    Add files via upload

--- JAVASCRIPT WORKS/DOM WORKS/SimpleCalculator/script.js
+++ JAVASCRIPT WORKS/DOM WORKS/SimpleCalculator/script.js
@@ -4,20 +4,24 @@ function performAddition(){
 
     let num2=+document.querySelector("#box2").value
 
+    // let num1=+box1.value
+
     let result=num1+num2
 
-    console.log(result);
+    // console.log(result);
+    document.querySelector("#root").innerHTML=`${num1}+${num2}=${result}`
 }
 
 function performSubtraction(){
 
-    let num1=+document.querySelector("#box1").value
+    let num1=+document.querySelector("#box1").value   //let num1=+box1.value
 
     let num2=+document.querySelector("#box2").value
 
     let result=num1-num2
 
-    console.log(result);
+    // console.log(result);
+    document.querySelector("#root").innerHTML=`${num1}-${num2}=${result}`
 }
 
 function performMultiplication(){
@@ -28,16 +32,28 @@ function performMultiplication(){
 
     let result=num1*num2
 
-    console.log(result);
+    document.querySelector("#root").innerHTML=`${num1}*${num2}=${result}`
+
+    // console.log(result);
 }
 
 function performDivision(){
 
-    let num1=+document.querySelector("#box1").value
+    // let num1=+document.querySelector("#box1").value
 
-    let num2=+document.querySelector("#box2").value
+    // let num2=+document.querySelector("#box2").value
 
+    let num1=+box1.value
+    let num2=+box2.value
     let result=num1/num2
-    
-    console.log(result);
+    document.querySelector("#root").innerHTML=`${num1}/${num2}=${result}`
+    // console.log(result);
+}
+
+function handleClick(event){
+    let operator=event.target.value;
+    let num1=box1.value
+    let num2=box2.value
+    let result=eval(`${num1}${operator}${num2}`)
+    document.querySelector("#root").innerHTML=`${num1}${operator}${num2}=${result}`
 }
